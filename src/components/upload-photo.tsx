998d5539be7d113a8f18db572f8cdd1e40d496c7
Elad <eladeladsaa@gmail.com>
2026-03-04 15:55:41 +0200

    Fix: add @types packages, fix blob image preview

--- src/components/upload-photo.tsx
+++ src/components/upload-photo.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState } from 'react';
-import Image from 'next/image';
 
 interface UploadPhotoProps {
   onUpload: (file: File) => void;
@@ -21,14 +20,12 @@ export default function UploadPhoto({ onUpload }: UploadPhotoProps) {
     <div className="flex flex-col items-center">
       <input type="file" accept="image/*" onChange={handleFileChange} />
       {previewUrl && (
-        <div className="mt-4 relative w-48 h-48">
-          <Image
-            src={previewUrl}
-            alt="תצוגה מקדימה"
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={previewUrl}
+          alt="תצוגה מקדימה"
+          className="mt-4 w-48 h-48 object-cover rounded-md"
+        />
       )}
     </div>
   );
